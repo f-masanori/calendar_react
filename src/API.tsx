e88b18976a2da47776425fb19021d71cb6d02417
masanori f <uramasanori0306@gmail.com>
2020-03-17 14:16:42 +0900

    微調整

--- src/API.tsx
+++ src/API.tsx
@@ -38,4 +38,124 @@ export const RegisterUser = (UID:string,email :string) => {
         }
     });
 }
-   
+
+/* Event削除 API */
+/* UID と　EventIDに基づいて削除 */
+/* UIDはjwt */
+/* res未実装 */
+export const DeleteEvent = (eventID: number) => {
+    console.log("DeleteEvent!!")
+    app.auth().currentUser?.getIdToken(true).then(async (idToken: any) => {
+        axios({
+            method: 'post',
+            url: APIURL + '/deleteEvent',
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': idToken
+            },
+            data: {
+                EventID: String(eventID)
+            }
+        }).then(res => {
+            // console.log(res.data.NextEventID)
+        }).catch(error => {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log('Error', error.message);
+            }
+        });
+    }).catch((error: any) => {
+        alert(error)
+    });
+}
+
+// export const GetNextEventID =()
+export const GetNextEventID =  ():any => {
+    console.log("GetNextEventID!!")
+    // let resEventIDxxx:number =999
+     app.auth().currentUser?.getIdToken(true).then((idToken: any) => {
+        axios({
+            method: 'post',
+            url: APIURL + '/getNextEventID',
+            headers: {
+                'Content-Type': "application/json",
+                 'Authorization': idToken
+            }
+        }).then(res => {
+            console.log(res.data.NextEventID)
+            return res.data.NextEventID
+            // resEventIDxxx = res.data.NextEventID
+        }).catch(error => {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log('Error', error.message);
+            }
+            return 1111
+        });
+    }).catch((error: any) => {
+        alert(error)
+    });
+    return 0
+
+
+}
+export const AddEvent = (_nextEventID: number, date: string, input: string | null) => { 
+     if (input == "" || input == null) {
+      
+     } else {
+        app.auth().currentUser?.getIdToken(true).then(async (idToken: any) => {
+        const res = await axios({
+            method: 'post',
+            url: APIURL + '/addEvent',
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': idToken
+            },
+            data: {
+                EventID: _nextEventID,
+                Date: date,
+                InputEvent: input
+            }
+        });
+    }).catch((error: any) => {
+        alert(error)
+    });
+    }
+    // app.auth().currentUser?.getIdToken(true).then(async (idToken: any) => {
+    //     const res = await axios({
+    //         method: 'post',
+    //         url: APIURL + '/addEvent',
+    //         headers: {
+    //             'Content-Type': "application/json",
+    //             'Authorization': idToken
+    //         },
+    //         data: {
+    //             EventID: _nextEventID,
+    //             Date: date,
+    //             InputEvent: input
+    //         }
+    //     });
+
+    //     // calendarApi.addEvent(
+    //     //   { 
+    //     //     id: _nextEventID,
+    //     //     title: input,
+    //     //     date: date
+    //     //   }
+    //     // )
+      
+    // }).catch((error: any) => {
+    //     alert(error)
+    // });
+    console.log("tete")
+}
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -15,15 +15,27 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { app } from './base';
 /* 設定ファイル */
 import APIURL from './Config'
-/* Bootstrap */
-import Container from 'react-bootstrap/Container'
+/* API */
+import * as API from './API' 
 
+/* Bootstrap */
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 const Calender = (history: any): JSX.Element => {
+  /* [dayGridPlugin, interactionPlugin]この制御するとエラーになる */
   const [calendarPlugins, setCalendarPlugins] = React.useState([dayGridPlugin, interactionPlugin])
   const { eventsContext, changeEvents } = useContext(EventContext);
   const [nextEventID,setNextEventID]=React.useState(0)
   let calendarRef: any = React.createRef()
+  let altnextEventID: number
+  altnextEventID = nextEventID
+  const [selectedEventID, setSelectedEventID] = useState(0);
+  const [selectedEventTitle, setSelectedEventTitle] = useState("0");
 
   /* /getEventsByUID からのGETで
   ログインユーザーのEventとnexteventidを取得 */
@@ -50,50 +62,118 @@ const Calender = (history: any): JSX.Element => {
           newEvents.push(temp)
         }
       }
+
+      altnextEventID = res.data.NextEventID
+      console.log(altnextEventID)
+
       setNextEventID(res.data.NextEventID)
       changeEvents(newEvents)
-      setCalendarPlugins([dayGridPlugin, interactionPlugin])
+      // setCalendarPlugins([dayGridPlugin, interactionPlugin])
     }).catch((error: any) => {
       alert(error)
     });
   }
 
-
   const addEvent = (props: any) => {
     console.log(props)
-    console.log("addEvendも")
-    let date = props.dateStr
-    let input = window.prompt(date + "の予定を入力してください", "");
-    let calendarApi = calendarRef.current.getApi()
-    
-    if (input == "" || input == null) {
-      alert("入力がありません。")
-    } else {
-      app.auth().currentUser?.getIdToken(true).then(async (idToken: any) => {
-        const res = await axios({
-          method: 'post',
-          url: APIURL + '/addEvent',
-          headers: {
-            'Content-Type': "application/json",
-            'Authorization': idToken
-          },
-          data: {
-            EventID: nextEventID,
-            Date: date,
-            InputEvent: input
-          }
-        });
-        calendarApi.addEvent(
-          { 
-            id: nextEventID,
-            title: input,
-            date: date
-          }
-        )
-      }).catch((error: any) => {
-        alert(error)
+    console.log("addEvend")
+    app.auth().currentUser?.getIdToken(true).then((idToken: any) => {
+      axios({
+        method: 'get',
+        url: APIURL + '/getNextEventID',
+        headers: {
+          'Content-Type': "application/json",
+          'Authorization': idToken
+        }
+      }).then(res => {
+        /* EventIDをGETで持ってくる */
+        console.log(res.data.NextEventID)
+        let NewEventID:number = res.data.NextEventID
+        let date = props.dateStr
+        let input = window.prompt(date + "の予定を入力してください", "");
+        let calendarApi = calendarRef.current.getApi()
+        if (input == "" || input == null) {
+          alert("入力がありません。")
+        } else {
+          /* DB変更処理API */
+          API.AddEvent(NewEventID, date, input)
+          /* FullCalendarAPI */
+          calendarApi.addEvent(
+            {
+              id: NewEventID,
+              title: input,
+              date: date
+            }
+          )
+          altnextEventID++
+        }
+      }).catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
       });
+    }).catch((error: any) => {
+      alert(error)
+    });
+  }
+
+  const editEvent = (info: any) => {
+
+    console.log("ID : "+info.event.id)
+    console.log("Title : " +info.event.title)
+
+    let calendarApi = calendarRef.current.getApi()
+    let eee = calendarApi.getEventById(info.event.id);
+    console.log(eee)
+    let allevents = calendarApi.getEvents()
+    console.log(allevents[2])
+
+    calendarApi.refetchEvents()
+    // changeEvents()
+    setSelectedEventID(info.event.id)
+    setSelectedEventTitle(info.event.title)
+    handleShow()
+  }
+  const deleteEvent = () => {
+    console.log("deleteEvent")
+    let calendarApi = calendarRef.current.getApi()
+    let _selectedEventID = calendarApi.getEventById(selectedEventID);
+    _selectedEventID.remove()
+    API.DeleteEvent(selectedEventID)
+  }
+  /* modal */
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  
+  const handleSubmit = (event: any) => {
+    let calendarApi = calendarRef.current.getApi()
+    console.log(eventsContext)
+    console.log(selectedEventTitle)
+    console.log(selectedEventID)
+
+    let newEvents = eventsContext
+    for (let i = 0; i < eventsContext.length; i++) {
+      console.log(i)
+      console.log(eventsContext[i].id)
+      if (eventsContext[i].id === selectedEventID) {
+        console.log(eventsContext[i].id)
+        newEvents[i].title = selectedEventTitle
+        // break
+      }
     }
+    changeEvents(newEvents)
+    event.preventDefault();
+  }
+  const handleChange = (event:any) =>{
+    setSelectedEventTitle(event.target.value );
   }
 
   useEffect(() => {
@@ -110,6 +190,7 @@ const Calender = (history: any): JSX.Element => {
     });
     console.log("useEffect end")
   }, []);
+
   return (
     <Container>
     <div>
@@ -121,10 +202,38 @@ const Calender = (history: any): JSX.Element => {
           events={eventsContext}
           selectable={true}
           selectMirror={true}
-          dateClick={(info) => addEvent(info)}
-          eventClick={(info) => alert(info.event.id)}
+          dateClick={(info) => { addEvent(info) }}
+          eventClick={(info) => { editEvent(info)}}
       />
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedEventID}
+          {selectedEventTitle}
+          <form onSubmit={handleSubmit}>
+            <label>
+              Title:
+          <input type="text" defaultValue={selectedEventTitle} onChange={handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+          <Button variant="primary" onClick={deleteEvent}>
+            Eventd削除
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
